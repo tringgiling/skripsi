637d12d585ffac4128bd8f7621b2a8382b392961
tringgiling <iqbalnugraha8989@gmail.com>
2021-03-25 10:29:42 +0700

    If else nya masih gagal wkwkw

--- anet.py
+++ anet.py
@@ -14,13 +14,21 @@ tes = subprocess.call("sudo ./pantau.sh", shell=True) ##bisa
 #print(cari_MAC)
 ############################################################
 
+# milih kolom ESSID nya pake satu spasi ya iqbaaaallllll :)
 file_csv = pd.read_csv("hasil_pantauan-01.csv", usecols=["BSSID"," ESSID"]) # Nampilin kolom yang diinginkan aja
-file_csv.dropna(inplace = True)			#nge drop Kolom kosong (Dalam hal ini, nyaring kolom ESSID, jadi dari station Mac kebawah datanya bakal di drop, jadi cuma AP doang yang tampil)
-#print (file_csv.loc[0])						# nampilin baris yang diinginkan
+file_csv.dropna(inplace = True)										#nge drop Kolom kosong (Dalam hal ini, nyaring kolom ESSID, jadi dari station Mac kebawah datanya bakal di drop, jadi cuma AP doang yang tampil)
 print(file_csv)
 
 AP_Dilindungi = input("\nSilahkan Pilih Akses Poin yang ingin dilindungi : ")
+list_AP = file_csv.values.tolist()										#Ngubah dataframe ke list supaya gampang if else nya
+
+if AP_Dilindungi in list_AP:											# Nge cek inputan user, tpi masih gagal
+	print("Adaaaa")
+else:
+	print("Tidaakk")
 
 
-stop_pantau = tes = subprocess.call("sudo ./stop_pantau.sh", shell=True)		# ngestop airodump, kalau semuanya udah beres
+
+
+stop_pantau = subprocess.call("sudo ./stop_pantau.sh", shell=True)		# ngestop airodump, kalau semuanya udah beres
 

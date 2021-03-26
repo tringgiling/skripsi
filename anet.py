f4cefa8d897fdc33d53ff0c40e2f30783fd306a8
tringgiling <iqbalnugraha8989@gmail.com>
2021-03-26 14:40:32 +0700

    Parameter parsing : OK, Variable py ke sh : BLUM

--- anet.py
+++ anet.py
@@ -1,31 +1,31 @@
 import re
 import subprocess
-import pandas as pd 																			# buat baca csv
+import pandas as pd 		
+
+### Sambutan																
 print ("Selamat Datang Di Anti Evil Twin !!!")
 print("Tunggu Sebentar, sedang melakukan scan AP\n")
 
-#pantau = subprocess.call("sudo ./pantau.sh", shell=True) 
-
-
-# milih kolom ESSID nya pake satu spasi ya iqbaaaallllll :)
-# dan isi dari kolom ESSID nya juga pada pake satu spasi wkwkwkwk bikin susah aja nih airodump
-
-file_csv = pd.read_csv("hasil_pantauan-02.csv", usecols=["BSSID"," ESSID"]) # Nampilin kolom yang diinginkan aja
+### Scan WIFI di sekitar
+pantau_awal = subprocess.call("sudo ./pantau.sh awal", shell=True) 			# Scanning di awal2 buat milih AP yang mau di lindungi	
+file_csv = pd.read_csv("hasil_pantauan-01.csv", usecols=["BSSID"," ESSID"]) # Nampilin kolom yang diinginkan aja
 file_csv.dropna(inplace = True)																#nge drop Kolom kosong (Dalam hal ini, nyaring kolom ESSID, jadi dari station Mac kebawah datanya bakal di drop, jadi cuma AP doang yang tampil)
 print(file_csv)
 
+### Memilih AP Yang ingin dilindungi
 AP_Dilindungi = input("\nSilahkan Pilih Akses Poin yang ingin dilindungi : ")
 AP_Dilindungi = " " + AP_Dilindungi														#Nge trik "satu spasi sebelum" dari isi kolom ESSID hasil Airodumb
 file_csv_BSSID = file_csv["BSSID"]														#Ngambil Kolom tertentu
 file_csv_ESSID = file_csv[" ESSID"]
-
 list_AP_BSSID = file_csv_BSSID.values.tolist()										#Ngubah dataframe ke list supaya gampang if else nya
 list_AP_ESSID = file_csv_ESSID.values.tolist()
-
-							
 index_ESSID = [i for i, e in enumerate(list_AP_ESSID) if e == AP_Dilindungi] #Ngambil No Index dari AP_Dilindungi buat dicocokin sama Index list_AP_BSSID
 MAC_AP_Dilindungi = [list_AP_BSSID[i] for i in index_ESSID]							#Ngambil Alamat MAC yang baris nya sama kaya AP_Dilindungi
 print (MAC_AP_Dilindungi)
+
+### Mulai Fokus memantau AP, dan mencari evil twin
+print("\nSedang mencoba memantau Wifi : " + AP_Dilindungi)
+pantau_fokus = subprocess.call("sudo ./pantau.sh {}", shell=True).format(AP_Dilindungi)		#Masuk sihh, cuman ngga bisa pake format, harus cari cara lain
 
 ######## WHILE - IF ELSE buat scanning evil twinn ############ 
 # if AP_Dilindungi in list_AP_ESSID and MAC_AP_Dilindungi !=:														# Nge cek inputan user, alhamdulillah udah bisa
@@ -35,5 +35,5 @@ print (MAC_AP_Dilindungi)
 
 
 
-#stop_pantau = subprocess.call("sudo ./stop_pantau.sh", shell=True)		# ngestop airodump, kalau semuanya udah beres
+stop_pantau = subprocess.call("sudo ./stop_pantau.sh", shell=True)		# ngestop airodump, kalau semuanya udah beres
 
--- pantau.sh
+++ pantau.sh
@@ -1,8 +1,19 @@
-#/bin/bash
+#!/bin/bash
 
-sudo airmon-ng start wlp10s0f0 # Mulai Mode Monitor
-tmux new -d # ngebuat session khusus buat nanti ngejalanin airodump-ng
-sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE wlp10s0f0mon ENTER #ngejalanin airodump di tmux session 0
+if [[ $1 == "awal" ]]						# buat milih AP yang mau dilindungi di awal2
+then
+	sudo airmon-ng start wlp10s0f0 # Mulai Mode Monitor
+	tmux new -d 								# ngebuat session khusus buat nanti ngejalanin airodump-ng
+	sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE wlp10s0f0mon ENTER #ngejalanin airodump di tmux session 0
+	echo "Tunggu bentar ok?" 
+	sleep 7s
 
-echo "Tunggu bentar ok?" 
-sleep 7s
+
+else 												# Mode fokus, Airodump cuma mantau AP dilindungi aja
+	sudo rm hasil_pantauan-01.*
+	tmux kill-session -t 0
+	tmux new -d
+	sudo tmux send -Rt 0 airodump-ng SPACE -N SPACE "$1" SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE wlp10s0f0mon ENTER #ngejalanin airodump di tmux session 0
+	echo "Yey Masukkk"
+	sleep 3s
+fi

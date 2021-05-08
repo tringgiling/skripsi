9af13a4801b68e24371afd13e1fafd896806e168
anet-test <iqbalnugraha8989@gmail.com>
2021-05-08 08:32:13 +0700

    tmux kill server, more better

--- pantau.sh
+++ pantau.sh
@@ -6,12 +6,12 @@ then
 	tmux new -d 								# ngebuat session khusus buat nanti ngejalanin airodump-ng
 	sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE  wlan0mon ENTER #ngejalanin airodump di tmux session 0 #wlp10s0f0mon
 	echo "Tunggu bentar ok?" 
-	sleep 7s
+	sleep 10s
 
 
 else 												# Mode fokus, Airodump cuma mantau AP dilindungi aja
 	sudo rm hasil_pantauan-01.*
-	tmux kill-session -t 0
+	tmux kill-server
 	tmux new -d
 	sudo tmux send -Rt 0 airodump-ng SPACE -N SPACE "'$1'" SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE wlan0mon  ENTER #ngejalanin airodump di tmux session 0 #wlp10s0f0mon
 	echo Melakukan Scaning Ke "$1" 

